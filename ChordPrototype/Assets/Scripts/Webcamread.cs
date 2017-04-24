da594f062234dfb153fbc5de8795389dab84d865
Bosco Bartilomo <bosco.bartilomo@gmail.com>
2017-04-24 17:04:55 +1000

    got the webcam reading colors and stuff good stuff m8

---
+++ ChordPrototype/Assets/Scripts/Webcamread.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public WebCamTexture input;
+    public Color[] data;
+
+    // Use this for initialization
+    void Start() {
+        //read from the webcam
+        WebCamDevice[] devices = WebCamTexture.devices;
+        //for (int i = 0; i < devices.Length; i++)
+        //{
+        //    Debug.Log(devices[i].name);
+        //}
+
+        Debug.Log("abcdef");
+        input = new WebCamTexture(devices[0].name);
+        Debug.Log("nopq");
+        input.Play();
+        Debug.Log("the fi");
+        //1080 by 1920
+        Color[] color;
+        data = new Color[32 * 32];
+        color = input.GetPixels(0, 0, 32, 32);
+        Debug.Log("the first color is: " + color[10].ToString()); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Process this puppy
+
+	}
+}
---
+++ ChordPrototype/Assets/Scripts/mainscript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainscript : MonoBehaviour
+{
+
+    public WebCamTexture input;
+    public Color[] data;
+
+    // Use this for initialization
+    void Start()
+    {
+        //read from the webcam
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+        }
+
+        Debug.Log("abcdef");
+        input = new WebCamTexture(devices[0].name);
+        Debug.Log("nopq");
+        input.Play();
+
+        Debug.Log("the fi");
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Process this puppy
+
+        //1080 by 1920
+        //Color[] color;
+        //Debug.Log(input.GetPixel(100, 100));
+
+        data = new Color[1 * 1];
+        data = input.GetPixels(100, 100, 1, 1);
+        Debug.Log("the first color is: " + data[0].ToString());
+    }
+}
