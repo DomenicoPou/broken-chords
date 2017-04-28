27fee4602078a4a6056c46948d80a70248d77051
Domenico poutanen <domenicopoutanen@gmail.com>
2017-04-28 13:01:36 +1000

    Changed camera to texture: to canvas connected to main camera

--- ChordPrototype/Assets/Scripts/changeMaterial.cs
+++ ChordPrototype/Assets/Scripts/changeMaterial.cs
@@ -1,26 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class changeMaterial : MonoBehaviour {
-
+    public RawImage rawimage;
 	// Use this for initialization
 	void Start () {
-        Debug.Log("change material!!!!");
         WebCamDevice[] devices = WebCamTexture.devices;
-
         // for debugging purposes, prints available devices to the console
         for (int i = 0; i < devices.Length; i++)
         {
             print("Webcam available: " + devices[i].name);
         }
-
-        Renderer rend = this.GetComponentInChildren<Renderer>();
-
         // assuming the first available WebCam is desired
         WebCamTexture tex = new WebCamTexture(devices[0].name);
-        rend.material.mainTexture = tex;
+        rawimage.texture = tex;
+        rawimage.material.mainTexture = tex;
         tex.Play();
     }
-
 }
